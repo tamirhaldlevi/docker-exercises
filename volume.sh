fd027f3b244791b7d975a1b1fb6e89c8e75c4708
Tamir <tamirhaldlevi@gmail.com>
2026-09-24 11:31:22 +0200

    efwe

--- volume.sh
+++ volume.sh
@@ -5,3 +5,8 @@ docker run --rm -v data:/data alpine sh -c "echo hej > /data/besked.txt"
 docker run --rm -v data:/data alpine cat /data/besked.txt
 docker volume rm data
 ! docker run --rm alpine cat /data/besked.txt
+docker run -d --name volume-site -v data:/data minside
+docker exec volume-site sh -c 'echo "Besked fra minside" > /data/minside.txt'
+docker rm -f volume-site
+
+docker run --rm -v data:/data minside cat /data/minside.txt
