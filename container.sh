d33aa62a349043dd42c6580d96c5f17f88923115
Tamir <tamirhaldlevi@gmail.com>
2026-09-24 10:09:23 +0200

    AHHHHHHH

--- container.sh
+++ container.sh
@@ -4,3 +4,7 @@ docker run -d --name min-container nginx
 docker exec min-container ls /usr/share/nginx/html
 docker logs min-container
 docker run -d --name min-store-container nginx
+docker stop min-container
+docker stop min-store-container
+docker rm min-container
+docker rm min-store-container
