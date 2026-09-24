fac03b52ba59120a816bf1269886e8def0d920c2
Tamir <tamirhaldlevi@gmail.com>
2026-09-24 11:24:24 +0200

    nu?

--- volume.sh
+++ volume.sh
@@ -3,4 +3,4 @@ set -e
 docker volume create data
 docker run --rm -v data:/data alpine sh -c "echo hej > /data/besked.txt"
 docker run --rm -v data:/data alpine cat /data/besked.txt
-docker volume rm besked-volume
+docker volume rm data
