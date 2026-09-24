b568deff70da742632fc28b3fa3a8147bfea4e95
Tamir <tamirhaldlevi@gmail.com>
2026-09-24 10:52:05 +0200

    NU PLS

--- image.sh
+++ image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 docker build -t minside .
-docker run -d --name site -p 8080:8000
+docker run -d --name site -p 8080:8000 minside
 sleep 2
 curl -f localhost:8080
 docker rm -f site
