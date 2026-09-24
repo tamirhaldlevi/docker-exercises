887b6833086ec05d4340dfb34a91a93ea68b9c9e
Tamir <tamirhaldlevi@gmail.com>
2026-09-24 11:27:40 +0200

    getit

--- volume.sh
+++ volume.sh
@@ -4,3 +4,4 @@ docker volume create data
 docker run --rm -v data:/data alpine sh -c "echo hej > /data/besked.txt"
 docker run --rm -v data:/data alpine cat /data/besked.txt
 docker volume rm data
+! docker run --rm alpine cat /data/besked.txt
